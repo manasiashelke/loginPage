7e3e743a46fac331f8fb2d8b3eff350dbb88ff5f
Manasi shelke <91251186+manasiashelke@users.noreply.github.com>
2022-01-27 14:59:49 +0530

    Add files via upload

---
+++ src/component/login.js
@@ -0,0 +1,112 @@
+import React from 'react'
+import './style.css';
+
+export default function login() {
+  const slideLeft = ()=>{
+    let container = document.getElementById("container")
+    container.classList.add("sign-up-mode");
+  }
+  
+  const slideRight = ()=>{
+    let container = document.getElementById("container")
+    container.classList.remove("sign-up-mode");
+  }
+  return (
+    <div>
+     <div id="container">
+      <div className="forms-container">
+        <div className="signin-signup">
+          <form action="/" className="sign-in-form">
+            <h2 className="title">Sign in</h2>
+            <div className="input-field">
+              <i className="fas fa-user"></i>
+              <input type="text" placeholder="Username" />
+            </div>
+            <div className="input-field">
+              <i className="fas fa-lock"></i>
+              <input type="password" placeholder="Password" />
+            </div>
+            <input type="submit" value="Login" className="btn solid" />
+            <p className="social-text">Or Sign in with social platforms</p>
+            <div className="social-media">
+              <a href="/" className="social-icon">
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a href="/" className="social-icon">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="/" className="social-icon">
+                <i className="fab fa-google"></i>
+              </a>
+              <a href="/" className="social-icon">
+                <i className="fab fa-linkedin-in"></i>
+              </a>
+            </div>
+          </form>
+          <form action="/" className="sign-up-form">
+            <h2 className="title">Sign up</h2>
+            <div className="input-field">
+              <i className="fas fa-user"></i>
+              <input type="text" placeholder="Username" />
+            </div>
+            <div className="input-field">
+              <i className="fas fa-envelope"></i>
+              <input type="email" placeholder="Email" />
+            </div>
+            <div className="input-field">
+              <i className="fas fa-lock"></i>
+              <input type="password" placeholder="Password" />
+            </div>
+            <input type="submit" className="btn" value="Sign up" />
+            <p className="social-text">Or Sign up with social platforms</p>
+            <div className="social-media">
+              <a href="/" className="social-icon">
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a href="/" className="social-icon">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="/" className="social-icon">
+                <i className="fab fa-google"></i>
+              </a>
+              <a href="/" className="social-icon">
+                <i className="fab fa-linkedin-in"></i>
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div className="panels-container">
+        <div className="panel left-panel">
+          <div className="content">
+            <h3>New here ?</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
+              ex ratione. Aliquid!
+            </p>
+            <button onClick={slideLeft} className="btn transparent" id="sign-up-btn">
+              Sign up
+            </button>
+          </div>
+          <img src="https://thetravelights.com/wp-content/uploads/2020/10/tech-web-app-development-1536x1536.png" className="image" alt="" />
+        </div>
+        <div className="panel right-panel">
+          <div className="content">
+            <h3>One of us ?</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              laboriosam ad deleniti.
+            </p>
+            <button onClick={slideRight}  className="btn transparent" id="sign-in-btn">
+              Sign in
+            </button>
+          </div>
+          <img src="https://www.07heavenmarketing.co.uk/hubfs/07_Shadowing_Image01-01.png" className="image" alt="" />
+        </div>
+      </div>
+    </div>
+ </div>
+  )
+}
+
